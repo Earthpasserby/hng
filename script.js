c1206a1875cacc94e09043a0e2f647a146bc0ac2
Samuel Iwuno <iwuno68@gmail.com>
2023-09-11 11:44:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function updateUTCTime() {
+  const utcTimeElement = document.getElementById("utc-time");
+
+  function updateTime() {
+    const currentTimeMillis = new Date().getTime();
+    const currentTimeUTC = new Date(currentTimeMillis).toUTCString();
+    utcTimeElement.innerHTML = currentTimeUTC;
+  }
+
+  updateTime();
+
+  setInterval(updateTime, 1000);
+}
+
+updateUTCTime();
+
+document.getElementById("refresh-btn").addEventListener("click", updateUTCTime);
